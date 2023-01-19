cfb4f1df441fa74ee1d8812e9033d19d7c4158ca
TyGosley <tygosley@gmail.com>
2023-01-18 21:40:32 -0800

    Step 15: show current time on load, update every second

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -1,4 +1,4 @@
-// TODO: save reference to important DOM elements using jQuery
+// ✅ save reference to important DOM elements using jQuery
 var timeDisplayEL = $("#time-display");
 var projectDisplayEL = $("#project-display");
 var projectFormEL = $("#project-form");
@@ -9,18 +9,19 @@ var projectTypeInputEl = $("#project-type-input");
 var projectDateInputEl = $("#project-date-input");
 
 
-// TODO: write a function to handle displaying the time
+// ✅ write a function to handle displaying the time
 function displayTime() {
-  // ...
+  var rightNow =dayjs().format("MMM DD, YYYY [at] hh:mm:ss a");
+  timeDisplayEL.text(rightNow);
 }
 
-// TODO: write a function to read projects from local storage
+// ✅ write a function to read projects from local storage
 function readProjectsFromStorage() {
   var projects = JSON.parse(localStorage.getItem("projects")) || [];
   return projects;
 }
 
-// TODO: write a function that saves projects (passed as a parameter) to local storage
+// ✅ write a function that saves projects (passed as a parameter) to local storage
 function saveProjectsToStorage(projects) {
   localStorage.setItem("projects", JSON.stringify(projects));
 }
@@ -31,16 +32,18 @@ function printProjectData() {
   projectDisplayEL.empty();
   
 var projects = readProjectsFromStorage();
-  // TODO: loop through each project and create a row
 
+// ✅ loop through each project and create a row
   for (var i = 0; i < projects.length; i++) {
     var project = projects[i];
-  
-    // ✅ get the date of the current project in the loop
+  // ✅ get the date of the current project in the loop
     var projectName = project.name;
     var projectType = project.type;
     var projectDate = dayjs(project.date);
-    // TODO: get date/time for START of today
+
+
+    // ✅ get date/time for START of today
+    var today = dayjs.startOf("day");
 
     // ✅ Create row and column elements (*hint!* <tr> -> <td>) for the project and add text to each cell
     var rowEl = $("<tr>");
@@ -50,20 +53,21 @@ var projects = readProjectsFromStorage();
 
 
     // ----- TASK 4 - ONLY WORK ON THIS IF YOU'VE COMPLETED TASKS 1 - 3!!!
-    // TODO: create a Delete button in its own table cell
+    // ✅ create a Delete button in its own table cell
     // (*hint!* save the index of the project as a `data-*` attribute on the button so we know
     // what project to delete when we click that button)
     var deleteEl = $('<td><button class="btn btn-sm">X</button></td>');
+
     // ----- TASK 4 - ONLY WORK ON THIS IF YOU'VE COMPLETED TASKS 1 - 3!!!
-    // TODO: add custom classes to the row to show if project is late or due today
+    // ✅ add custom classes to the row to show if project is late or due today
     // (*hint!* comparing project date to today's date)
-    if ('project is late') {
-      // project is late
-    } else if ('project is do today') {
-      // project is do today
+    if (projectDate.isBefore(today)) {
+      rowEl.addClass("project-late");
+    } else if (projectDate.isSame(today)) {
+      rowEl.addClass("project-today");
     }
 
-    // TODO: append elements to DOM to display them
+    // ✅ append elements to DOM to display them
   // } // end for loop
   rowEl.append(nameEl, typeEl, dateEl, deleteEl);
   projectDisplayEL.append(rowEl);
@@ -74,25 +78,28 @@ var projects = readProjectsFromStorage();
 function handleProjectFormSubmit(event) {
   event.preventDefault();
   
-  // TODO: read user input from the form
+  // ✅ read user input from the form
 var projectName = projectNameInputEl.val().trim();
 var projectName = projectTypeInputEl.val();
 var projectName = projectDateInputEl.val();
-  // TODO: mold a new object with the data from above
+  // ✅ mold a new object with the data from above
 var newProject = {
   name: projectName,
   type: projectType,
   date: projectDate
 }
 
-  
+  // ✅: add project to local storage w/ the function you already wrote
+  // (*hint!* make sure it doesn't overwrite other projects, but rather ADDS it)
 var projects = readProjectsFromStorage(); 
 projects.push(newProject);
 
 saveProjectsToStorage(projects)
   
+// ✅: print project data with the function you wrote
 printProjectData();
   
+// ✅: clear the form inputs
   projectNameInputEl.val("");
   projectTypeInputEl.val("");
   projectDateInputEl.val("");
@@ -110,8 +117,8 @@ projectFormEL.on("submit", handleProjectFormSubmit);
 // (*hint!* brush up on jQuery event delegation to listen for clicks on dynamically elements
 
 // TODO: call the function to display the initial time
-
+displayTime();
 // TODO: update the time every second
-
+setInterval(displayTime, 1000);
 // TODO: call the function to print the project data read from local storage on page load
 printProjectData();
